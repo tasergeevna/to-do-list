82e9b49973b15b100c06f1de47acca8ff622c051
Tatsiana Yasiuchenia <70094257+tasergeevna@users.noreply.github.com>
2021-10-16 22:45:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const addTaskButton = document.querySelector(".add-task");
+const addListButton = document.querySelector(".add-list");
+const taskInput = document.querySelector(".task-input");
+const listInput = document.querySelector(".list-input");
